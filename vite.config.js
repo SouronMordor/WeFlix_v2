0534626c0f0d30ddc10c8f5177056ee090ecd83d
souron <souronmordor1@gmail.com>
2026-07-05 19:34:31 +0300

    change base for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // vite.config.js
-  server: {
-    host: '0.0.0.0' // Expose on all network interfaces
-  }
+  base: '/tv/'
 
 
 })
